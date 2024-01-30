eddf1de518cdc1631440e77dda476af04fd4f315
--global <botka1998@gmail.com>
2024-01-30 19:52:06 +0100

    add some real tests to run_client

--- functions/run_client.py
+++ functions/run_client.py
@@ -2,6 +2,9 @@ from abb import Robot
 
 if __name__ == "__main__":
     robot = Robot("0.0.0.0", port_motion=5000)
-    while True:
-        input("Press enter to send")
-        robot.send("hey")
+    input("Press enter to send")
+    robot.get_joints()
+    input("Press enter to send")
+    robot.set_cartesian([[0, 0, 0], [1, 0, 0, 0]])
+    input("Press enter to send")
+    robot.send("hey")
