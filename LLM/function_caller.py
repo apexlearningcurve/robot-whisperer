d8fa9aa4de9ff6cb553ee5ece0132560f454cfdb
--global <studen.m98@gmail.com>
2024-07-04 21:54:57 +0200

    Update poetry files.

--- LLM/function_caller.py
+++ LLM/function_caller.py
@@ -4,13 +4,15 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from jsonformer import Jsonformer
 
-from LLM.prompts import classifier_prompt_1
-from LLM.action_models import generic_function
+from prompts import classifier_prompt_1
+from action_models import generic_function
 
 
 class FunctionCaller:
     def __init__(
-        self, model_name: str = "TheBloke/Mistral-7B-Instruct-v0.2-GPTQ"
+        self,
+        model_name: str = "TheBloke/Mistral-7B-Instruct-v0.2-GPTQ",
+        use_jsonformer: bool = True,
     ) -> None:
         self.device = (
             torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -31,11 +33,24 @@ class FunctionCaller:
             device=self.device,
             cache_dir=weights_dir,
         )
-
-        self.jsonformer = Jsonformer(self.llm, self.tokenizer, generic_function, "")
+        self.jsonformer = None
+        if use_jsonformer:
+            self.jsonformer = Jsonformer(self.llm, self.tokenizer, generic_function, "")
 
     def generate(self, prompt):
-        self.jsonformer.prompt = classifier_prompt_1.format(user_query=prompt)
-        fn_call = self.jsonformer()
-
-        return fn_call
+        if self.jsonformer is not None:
+            self.jsonformer.prompt = classifier_prompt_1.format(user_query=prompt)
+            return self.jsonformer()
+        else:
+            tokens = self.tokenizer(
+                prompt_template, return_tensors="pt"
+            ).input_ids.cuda()
+            generation_output = self.llm.generate(
+                tokens,
+                do_sample=True,
+                temperature=0.1,
+                top_p=0.95,
+                top_k=40,
+                max_new_tokens=512,
+            )
+            return self.tokenizer.decode(generation_output[0])
